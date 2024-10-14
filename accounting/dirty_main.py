08b915bdcc0a1ea6ae08067282f28ca6f910c154
sibirb <oxothuknsk@gmail.com>
2024-10-14 16:01:51 +0300

    Схема бухгалтерия

---
+++ accounting/dirty_main.py
@@ -0,0 +1,8 @@
+from application.salary import *
+from application.db.people import *
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(f"Текущая дата: {datetime.now()}")
+    calculate_salary()
+    get_employees()
